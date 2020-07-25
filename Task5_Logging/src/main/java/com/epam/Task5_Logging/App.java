9740f53ce6279da097b0fa75461c3eea971d7bc2
alesrinija <60352670+alesrinija@users.noreply.github.com>
2020-07-25 21:26:45 +0530

    first commit

---
+++ Task5_Logging/src/main/java/com/epam/Task5_Logging/App.java
@@ -0,0 +1,12 @@
+package com.epam.Task5_Logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+public class App 
+{
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        LOGGER.info("Welcome to my Universe!!!!");
+    }
+}
